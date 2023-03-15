c417cc49d5f917d39eab8744962f6d0284ed4603
Femure <maxime.femery@gmail.com>
2023-03-15 22:14:59 +0700

    Debug Oauth2

--- src/main/java/ku/project/config/SecurityConfig.java
+++ src/main/java/ku/project/config/SecurityConfig.java
@@ -1,13 +1,10 @@
 package ku.project.config;
 
-import ku.project.service.UserDetailsServiceImp;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
