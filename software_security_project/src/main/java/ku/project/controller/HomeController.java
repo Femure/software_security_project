a15f034d0cf64b07e14efbc13f97ff098a161c0d
Femure <maxime.femery@gmail.com>
2023-03-28 15:02:32 +0700

    Add policy page and correct header

--- software_security_project/src/main/java/ku/project/controller/HomeController.java
+++ software_security_project/src/main/java/ku/project/controller/HomeController.java
@@ -12,4 +12,14 @@ public class HomeController {
        model.addAttribute("greeting", "Welcome to project Website");
        return "home";  // name of a html template: home.html
    }
+   
+   @RequestMapping("/policy")
+   public String getPolicyPage() {
+       return "policy";  // name of a html template: home.html
+   }
+
+   @RequestMapping("/")
+   public String getRootPage() {
+       return "redirect:/home"; 
+   }
 }
